e1115a21d25f90edbde60ce05fbb633161f582dc
joelcoronah <joelcoronah@gmail.com>
2024-08-02 17:37:56 -0400

    :hammer: feat: add about link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <span className="text-6xl"> Hola mundo </span>
+      <Link href="/about">Ir a About</Link>
     </main>
   );
 }
